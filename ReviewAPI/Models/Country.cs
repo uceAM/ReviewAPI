3c7650064397cf8ae31705f158409a36e2ab25ae
uceAM <arshadm.2406@gmail.com>
2023-10-28 12:12:11 +0530

    adds models

---
+++ ReviewAPI/Models/Country.cs
@@ -0,0 +1,8 @@
+﻿namespace ReviewAPI.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ReviewAPI/Models/Reviewer.cs
@@ -0,0 +1,9 @@
+﻿namespace ReviewAPI.Models
+{
+    public class Reviewer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
