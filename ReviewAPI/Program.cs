7fb9c9b408289da7176e309fece81214cf2673aa
uceAM <arshadm.2406@gmail.com>
2023-10-28 16:19:55 +0530

    adds EF tools, migrations and seed consumption logic

--- ReviewAPI/Program.cs
+++ ReviewAPI/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using ReviewAPI;
 using ReviewAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddTransient<Seed>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,7 +19,19 @@ builder.Services.AddDbContext<DataContext>(options =>
 });
 
 var app = builder.Build();
-
+if(args.Length == 1 && args[0].ToLower() == "seeddata")
+{
+    SeedData(app);
+}
+void SeedData(IHost app)
+{
+    var scopedFactoy = app.Services.GetService<IServiceScopeFactory>();
+    using(var scope= scopedFactoy.CreateScope())
+    {
+        var service = scope.ServiceProvider.GetService<Seed>();
+        service.SeedDataContext();
+    }
+}
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
